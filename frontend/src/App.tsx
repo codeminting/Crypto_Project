c40c2e2380fb1496e3b0aa71c5a448c1aee0ae9d
Yudish Juwaheer <60716609+yjuwaheer@users.noreply.github.com>
2022-08-30 19:04:59 -0230

    Frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// Chakra UI
+import { Button } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx,jsx,ts,md,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
