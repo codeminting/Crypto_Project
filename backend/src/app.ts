9a2832f2467772cb1cda1a6cac56742d4ea5773d
plouuf <71836826+plouuf@users.noreply.github.com>
2022-08-31 16:06:17 -0230

    port fix :D

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 // DB
 import pool from "../configs/db";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 // Routes
 import homeRouter from "../routes/home";
 
