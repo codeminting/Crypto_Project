f074e9c356e433e9bb0d653893a7e2928940771b
Yudish Juwaheer <60716609+yjuwaheer@users.noreply.github.com>
2022-08-31 16:14:35 -0230

    Updated navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,15 @@
-import { Box, Button } from '@chakra-ui/react';
-import { NavLink as Link } from 'react-router-dom';
+import { Button } from "@chakra-ui/react";
+import { NavLink as Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <Box className="flex w-full h-20 px-20 justify-between items-center">
-      <Box>
+    <div className="flex w-full h-20 px-20 justify-between items-center">
+      <div>
         <Link to="/" className="font-bold text-2xl text-center">
           Code Minting
         </Link>
-      </Box>
-      <Box className="flex justify-evenly">
+      </div>
+      <div className="flex justify-evenly">
         <Link to="/menu-link1" className="mx-2">
           Menu Link
         </Link>
@@ -19,16 +19,21 @@ const Navbar = () => {
         <Link to="/menu-link3" className="mx-2">
           Menu Link
         </Link>
-      </Box>
-      <Box>
+      </div>
+      <div>
         <Link to="/sign-up" className="mx-2">
           <Button>Sign Up</Button>
         </Link>
         <Link to="/log-in" className="mx-2">
           <Button>Log In</Button>
         </Link>
-      </Box>
-    </Box>
+        {false && (
+          <Link to="/log-in" className="mx-2">
+            <Button>Log Out</Button>
+          </Link>
+        )}
+      </div>
+    </div>
   );
 };
 
