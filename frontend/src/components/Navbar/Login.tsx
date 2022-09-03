ffeea837f596b3c10ebd2bc647b7da53f81bea59
Muhammad Uwais Jahmeerbacus <uzi241299@gmail.com>
2022-09-03 20:50:52 -0230

    changed login component from drawer to modal

--- frontend/src/components/Navbar/Login.tsx
+++ frontend/src/components/Navbar/Login.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import {
-  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
   Button,
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
   FormControl,
   FormLabel,
+  Input,
   FormHelperText,
   FormErrorMessage,
   FormErrorIcon,
@@ -22,7 +22,6 @@ import { TiDeleteOutline } from "react-icons/ti";
 import { MdPassword } from "react-icons/md";
 import { HiOutlineMail } from "react-icons/hi";
 
-// Interface
 interface LoginPropsTypes {
   isOpen: boolean;
   onOpen: () => void;
@@ -31,52 +30,48 @@ interface LoginPropsTypes {
 
 const Login = ({ isOpen, onClose }: LoginPropsTypes) => {
   return (
-    <Drawer isOpen={isOpen} placement="right" onClose={onClose} size={"sm"}>
-      <DrawerOverlay />
-      <DrawerContent>
-        <DrawerCloseButton />
-        <DrawerHeader>Log In</DrawerHeader>
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Login</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* <Input placeholder="Type here..." />
+             */}
+            <FormControl className="mb-5" isRequired isInvalid>
+              <FormLabel>
+                <HiOutlineMail className="inline mr-1" />
+                Email Address
+              </FormLabel>
+              <Input type="email" />
+              {/* <FormHelperText>Please Enter Your Email</FormHelperText> */}
+              <FormErrorMessage>
+                <FormErrorIcon />
+                This field is required
+              </FormErrorMessage>
+            </FormControl>
 
-        <DrawerBody>
-          {/* <Input placeholder="Type here..." />
-           */}
-          <FormControl className="mb-5" isRequired isInvalid>
-            <FormLabel>
-              <HiOutlineMail className="inline mr-1" />
-              Email Address
-            </FormLabel>
-            <Input type="email" />
-            {/* <FormHelperText>Please Enter Your Email</FormHelperText> */}
-            {/* <FormErrorMessage>
-              <FormErrorIcon />
-              This field is required
-            </FormErrorMessage> */}
-          </FormControl>
+            <FormControl className="mb-5" isRequired>
+              <FormLabel>
+                <MdPassword className="inline mr-1" /> Password
+              </FormLabel>
+              <Input type="password" />
+            </FormControl>
+          </ModalBody>
 
-          <FormControl className="mb-5" isRequired>
-            <FormLabel>
-              {" "}
-              <MdPassword className="inline mr-1" /> Password
-            </FormLabel>
-            <Input type="password" />
-          </FormControl>
-
-          <div className="flex justify-end">
-            
-          </div>
-        </DrawerBody>
-
-        <DrawerFooter>
-          <Button mr={5} size="lg" rightIcon={<TiDeleteOutline size={20} />}>
+          <ModalFooter>
+            <Button colorScheme={"red"} mr={5} size="lg" rightIcon={<TiDeleteOutline size={20} />}>
               Reset
             </Button>
 
-            <Button size="lg" rightIcon={<IoMdLogIn size={20} />}>
+            <Button colorScheme={"blue"} size="lg" rightIcon={<IoMdLogIn size={20} />}>
               Log In
             </Button>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
